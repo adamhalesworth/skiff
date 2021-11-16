3eb1778bfe4c7364f874497c2f6479f6cf31914d
Adam Halesworth <a.halesworth@gmail.com>
2021-11-16 11:01:42 +0000

    Metadata updates and tidying

--- lib/skiff.dart
+++ lib/skiff.dart
@@ -1,3 +1,11 @@
+/// A lightweight, Future-based library for expressing CQRS principles in Dart
+/// projects.
+///
+/// This package allows you to write smaller, more robust and testable code by
+/// turning data access (or anything else you can think of) logic into first-class
+/// objects with sound typing.
+library skiff;
+
 export 'src/commands/command.dart';
 export 'src/commands/command_handler.dart';
 export 'src/commands/command_result.dart';
